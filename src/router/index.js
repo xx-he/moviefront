475e4444b34f67b1fc1b11cafb1ecc7463275903
lixue <2452636086@qq.com>
2020-04-23 11:19:25 +0800

    MOD：去掉登录跳转

--- src/router/index.js
+++ src/router/index.js
@@ -108,21 +108,3 @@ export default router;
 
 // 同步 localstorage 信息到 store
 store.commit(types.SYNC);
-
-// 路由导航守卫
-// router.beforeEach((to, from, next) => {
-//   if (to.matched.some(routerObjectEle => routerObjectEle.meta.requireLogin)) {
-//     if (store.state.log_status) {
-//       next();
-//     } else {
-//       next({
-//         path: '/login',
-//         query: {
-//           redirect: to.fullPath,
-//         },
-//       });
-//     }
-//   } else {
-//     next();
-//   }
-// });
